5e1d186aef9ea99a0213fff6278bcbb3495dceb4
Pedro <pedrocarsamar@gmail.com>
2024-08-16 12:01:56 -0300

    Finalização.

--- ideological_distribution.R
+++ ideological_distribution.R
@@ -26,13 +26,23 @@ party_seats <- party_seats %>%
             copy = T) %>% 
   select(-year)
 
+View(party_seats %>% filter(is.na(ideo)))
+
 party_seats %>% 
   filter(is.na(ideo)) %>% 
   distinct(party)
 
+party_seats %>% 
+  filter(is.na(ideo)) %>% 
+  group_by(city_ibge) %>% 
+  summarise(soma = sum(party_seat_share),
+            proporcao = sum(party_seat_share/disp_seats)) %>% 
+  View()
+
 ave_ideo <- party_seats %>% filter(!is.na(ideo)) %>% 
   group_by(city_ibge) %>% 
-  reframe(ideo_mean = sum(party_seat_share * ideo / disp_seats))
+  mutate(notna_seats = sum(party_seat_share)) %>% 
+  reframe(ideo_mean = sum(party_seat_share * ideo / notna_seats))
 
 max(ave_ideo$ideo_mean)
 min(ave_ideo$ideo_mean)
@@ -63,3 +73,70 @@ ggplot() +
 # 1990: PDC/DC
 
 # Em todos os casos, não há dados para PRTB, PMN, PMB, PATRIOTA, AVANTE
+
+## Usando dados de Bolognesi et al. (2021) -------------------------------------
+
+# Um ano após aplicação do survey, PRB foi renomeado pra REPUBLICANOS (REP)
+ideo_b <- data.frame(party = c("PSTU", "PCO", "PCB", "PSOL", "PCDOB", "PT", "PDT",
+                     "PSB", "REDE", "PPS", "PV", "PTB", "AVANTE",
+                     "SD", "PMN", "PMB", "PHS", "MDB", "PSD", "PSDB",
+                     "PODE", "PPL", "PRTB", "PROS", "PRP", "REP", "PR",
+                     "PTC", "DC", "PSL", "NOVO", "PP", "PSC", "PATRIOTA", "DEM"),
+           ideob.mean = c(0.51, 0.61, 0.91, 1.28, 1.92, 2.97, 3.92,
+                    4.05, 4.77, 4.92, 5.29, 6.1, 6.32,
+                    6.5, 6.88, 6.9, 6.96, 7.01, 7.09, 7.11,
+                    7.24, 7.27, 7.45, 7.47, 7.59, 7.78, 7.78,
+                    7.86, 8.11, 8.11, 8.13, 8.20, 8.33, 8.55, 8.57)) %>% 
+  mutate(ideo.b = case_when(
+    ideob.mean <= 1.5 ~ -3,
+    ideob.mean >= 1.51 & ideob.mean <= 3 ~ -2,
+    ideob.mean >= 3.01 & ideob.mean <= 4.49 ~ -1,
+    ideob.mean >= 4.5 & ideob.mean <= 5.5 ~ 0,
+    ideob.mean > 5.5 & ideob.mean < 7.01 ~ 1,
+    ideob.mean > 7 & ideob.mean <= 8.5 ~ 2,
+    ideob.mean >= 8.49 ~ 3))
+
+party_seats.b <- party_seats %>% 
+  left_join(ideo_b, join_by(party == party),
+            copy = T)
+
+View(party_seats %>% filter(is.na(ideo)))
+
+party_seats.b %>% 
+  filter(is.na(ideo.b)) %>% 
+  distinct(party)
+
+party_seats %>% 
+  filter(is.na(ideo)) %>% 
+  group_by(city_ibge) %>% 
+  summarise(soma = sum(party_seat_share),
+            proporcao = sum(party_seat_share/disp_seats)) %>% 
+  View()
+
+ave_ideo <- party_seats %>% filter(!is.na(ideo)) %>% 
+  group_by(city_ibge) %>% 
+  mutate(notna_seats = sum(party_seat_share)) %>% 
+  reframe(ideo_mean = sum(party_seat_share * ideo / notna_seats))
+
+max(ave_ideo$ideo_mean)
+min(ave_ideo$ideo_mean)
+
+ideo_br <- read_municipality(year=2020) %>% 
+  left_join(ave_ideo, join_by(code_muni==city_ibge))
+
+ggplot() +
+  geom_sf(data=ideo_br, aes(fill=ideo_mean), size=.15) +
+  scale_fill_gradientn(colors = c(low = "red", mid = "white", high = "blue"))
+
+ideo_ce <- read_municipality(code_muni = 23, year=2020) %>% 
+  left_join(ave_ideo, join_by(code_muni==city_ibge))
+
+ggplot() +
+  geom_sf(data=ideo_ce, aes(fill=ideo_mean), color= NA, size=.15)
+
+ideo_pb <- read_municipality(code_muni = 25, year=2020) %>% 
+  left_join(ave_ideo, join_by(code_muni==city_ibge))
+
+ggplot() +
+  geom_sf(data=ideo_pb, aes(fill=ideo_mean), size=.15) +
+  scale_fill_gradientn(colors = c(low = "red", mid = "white", high = "blue"))
